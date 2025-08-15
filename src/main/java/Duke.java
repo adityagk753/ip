cfc80a4dafba40e66730e95def99d972c134d8bd
adityagk753 <adityagk753@gmail.com>
2025-08-15 19:39:45 +0800

    Complete Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,11 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, I'm Cody");
         System.out.println("What can I do for you?");
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
     }
